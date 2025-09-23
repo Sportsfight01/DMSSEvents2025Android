6dde311d083c2e49e2a60a47efed6231ef32897f
Durgaprasadganti <119028817+Durgaprasadganti@users.noreply.github.com>
2025-09-23 16:31:21 +0530

    version 3.0 build

---
+++ app/src/main/java/com/digitalminds/dmssevent/ComplaintsActivity.java
@@ -0,0 +1,24 @@
+package com.digitalminds.dmssevent;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+
+public class ComplaintsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+}
